9b3ad03aeb7505677bed9fab269134fdaafedaae
Shah Danish <shah.official1011@gmail.com>
2023-11-12 21:29:35 +0530

    Bug resolved

--- src/components/TopicsList.jsx
+++ src/components/TopicsList.jsx
@@ -1,46 +1,58 @@
-import RemoveBtn from "./RemoveBtn";
-import { HiPencilAlt } from "react-icons/hi";
-import Link from "next/link";
+'use client';
 
-const getTopics = async() => {
-    try {
-        const res = await fetch('http://localhost:3000/api/topics', {
-            cache: 'no-store',
-        });
+import { useEffect, useState } from 'react';
+import RemoveBtn from './RemoveBtn';
+import { HiPencilAlt } from 'react-icons/hi';
+import Link from 'next/link';
 
-        if(!res.ok){
-            throw new Error("Failed to fetch topics");
-        }
+const getTopics = async () => {
+  try {
+    const res = await fetch('http://localhost:3000/api/topics', {
+      cache: 'no-store',
+    });
 
-        return res.json();
-    } catch (error) {
-        console.log("Error loading topics: ", error);
+    if (!res.ok) {
+      throw new Error('Failed to fetch topics');
     }
-}
 
-export default async function TopicsList(){
+    return res.json();
+  } catch (error) {
+    console.log('Error loading topics: ', error);
+    return { topics: [] };
+  }
+};
 
-    const { topics } = await getTopics();
+export default function TopicsList() {
+  const [topics, setTopics] = useState([]);
 
-    return (
-        <>
-        {topics.map((t) => ( 
-        <div className="mt-4 p-5 flex justify-between rounded-md bg-slate-700">
-            <div>
-                <h3 className="text-2xl font-bold">{t.title}</h3>
-                <div className="mt-2">{t.description}</div>
-            </div>
+  useEffect(() => {
+    const fetchTopics = async () => {
+      const { topics } = await getTopics();
+      setTopics(topics);
+    };
 
-            <div className="flex gap-4 items-start">
-                <RemoveBtn/>
-                <button className="text-green-500">
-                    <Link href={`/editTopic/${t._id}`}>
-                        <HiPencilAlt size={24}/>
-                    </Link>
-                </button>
-            </div>
+    fetchTopics();
+  }, []);
+
+  return (
+    <>
+      {topics.map((t) => (
+        <div key={t._id} className="mt-4 p-5 flex justify-between rounded-md bg-slate-700">
+          <div>
+            <h3 className="text-2xl font-bold">{t.title}</h3>
+            <div className="mt-2">{t.description}</div>
+          </div>
+
+          <div className="flex gap-4 items-start">
+            <RemoveBtn />
+            <button className="text-green-500">
+              <Link href={`/editTopic/${t._id}`}>
+                <HiPencilAlt size={24} />
+              </Link>
+            </button>
+          </div>
         </div>
-        ))}
-        </>
-    )
+      ))}
+    </>
+  );
 }
