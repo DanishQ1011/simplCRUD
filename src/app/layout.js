ca48a8ff384f4d37409915d70f326f1a6b3cf0e2
Shah Danish <shah.official1011@gmail.com>
2023-11-13 17:41:32 +0530

    Added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel='shortcut icon' href='/favicon/favicon.ico'/>
+      </head>
       <body className={`${inter.className} bg-gray-900 text-white mx-[250px]`}>
         <Navbar/>
         {children}</body>
